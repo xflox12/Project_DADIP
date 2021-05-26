76e5035b81e829ac47db1caa518569cc224d52fb
Florian Schietinger <schiflo5991@gmail.com>
2021-05-26 15:56:46 +0200

    Include Appseed Atlantis Template into Project

--- theStartpageApp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Test(models.Model):
-    test_id = models.IntegerField(default=0)
-    test_text = models.CharField(max_length=200)
--- theStartpageApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('home/', views.home_view_temp, name='home'),
-]
