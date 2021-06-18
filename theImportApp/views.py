ef278ff83514ccec4c8333b3bcac2d63a2c03a39
Marco <marco.thiessen@stud-mail.uni-wuerzburg.de>
2021-06-18 14:32:05 +0200

    Save uploaded files to sqlite3 database

--- theImportApp/views.py
+++ theImportApp/views.py
@@ -15,31 +15,31 @@ import sqlite3
 
 def fileimport_view(httprequest, *args, **kwargs):
     """Do  anything with request"""
-    #return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse("Hello, world. You're at the polls index.")
     print("##### Inside fileimport_view")
     return render(httprequest, "myTemplates/fileimport.html")
 
 
 def upload_func(HttpRequest):
-    #get current Date and Time for timestamp
+    # get current Date and Time for timestamp
     _datetime = datetime.now()
-    datetime_str = _datetime.strftime("%Y-%m-%d") # mit Uhrzeit: datetime_str = _datetime.strftime("%Y-%m-%d-%H-%M-%S")
+    datetime_str = _datetime.strftime("%Y-%m-%d_%H-%M")  # mit Uhrzeit: datetime_str = _datetime.strftime("%Y-%m-%d-%H-%M-%S")
 
     if HttpRequest.method == 'POST' and HttpRequest.FILES.get('newFile'):
         myFile = HttpRequest.FILES.get('newFile')
         fs = FileSystemStorage()
 
-        #Split filename to add timestamp to the name and save parts of the name
+        # Split filename to add timestamp to the name and save parts of the name
         # if there are more than one dots create a list and use last part to remove extension
         file_name_split = myFile.name.split('.')
         file_name_list = file_name_split[:-1]
         ext = file_name_split[-1]
         file_name_without_ext = '.'.join(file_name_list)
 
-        #Create new filenmae with timestamp
-        myFile.name = file_name_without_ext+'_'+datetime_str+'.'+ext
+        # Create new filenmae with timestamp
+        myFile.name = file_name_without_ext + '_' + datetime_str + '.' + ext
 
-        #save file in core/uploadStorage folder
+        # save file in core/uploadStorage folder
         filename = fs.save(myFile.name, myFile)
         uploaded_file_url = fs.url(filename)
 
@@ -51,8 +51,11 @@ def upload_func(HttpRequest):
         print(myFile)
         print(uploaded_file_url)
 
-        #parse file into database via pandas
-        #pandas_func(uploaded_file_url)
+        # parse file into database via pandas
+        # pandas_func(uploaded_file_url)
+
+        # df = pd.read_excel("myFile", engine = 'openpyxl')
+
 
     else:
         print('Error. Wrong method!')
@@ -61,15 +64,16 @@ def upload_func(HttpRequest):
         }
     return render(HttpRequest, "myTemplates/fileimport.html", context)
 
-"""Später Übergabeparameter einfügen: filepath"""
-def pandas_func(HttpRequest):
 
+"""Später Übergabeparameter einfügen: filepath"""
+
+
+def pandas_func(HttpRequest):
     print('\n##### Start Parsing File...')
 
-
     filepath = 'core/uploadStorage/EKKO_2021-06-10.XLSX'
-    #pd.DataFrame()
-    #df = pd.read_csv(filepath, sep=";")
+    # pd.DataFrame()
+    # df = pd.read_csv(filepath, sep=";")
     df = pd.read_excel(filepath, engine='openpyxl')
 
     print(df)
@@ -78,31 +82,44 @@ def pandas_func(HttpRequest):
 
     """How to parse via pandas: https://www.geeksforgeeks.org/drop-empty-columns-in-pandas/"""
     nan_value = float("NaN")
-    df.replace("", nan_value, inplace=True)     #replace all empty places with null
-    df.replace(0, nan_value, inplace=True)      #replace all zeros with null
+    df.replace("", nan_value, inplace=True)  # replace all empty places with null
+    df.replace(0, nan_value, inplace=True)  # replace all zeros with null
 
     df.dropna(how='all', axis=1, inplace=True)  # remove all null value columns
 
+    # Remove all rows which have at least one null value
+    # new_df = df.dropna(axis = 0, how = 'any', inplace = True)
 
     print(df)
 
     print('##### ... Parsing finished!\n')
 
-    #conn = sqlite3.connect('TestDB1.db')
-    #c = conn.cursor()
+    conn = sqlite3.connect('TestDB1.db')
+    c = conn.cursor()
 
-    #c.execute('CREATE TABLE FRAUDS (Col1 text, Col2 number)')
-    #conn.commit()
+    # check if FRAUD table exists
+    c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='FRAUDS' ''')
 
-    #df.to_sql('FRAUDS', conn, if_exists='replace', index=False)
+    # if the count is 1, then table exists
+    if c.fetchone()[0] == 1:
 
-    #c.execute('''
-    #SELECT * FROM FRAUDS
-     #         ''')
+            # replace the current table with new data from dataframe
+            df.to_sql('FRAUDS', conn, if_exists='replace', index=False)
+            conn.commit()
+    # if table does not exist, create a new table and import data from dataframe
+    else:
+        c.execute('CREATE TABLE FRAUDS (Col1 text, Col2 number)')
+        df.to_sql('FRAUDS', conn, if_exists='replace', index=False)
+        conn.commit()
 
-    #for row in c.fetchall():
-        #print(row)
+    c.execute('''
+        SELECT * FROM FRAUDS
+              ''')
+
+    for row in c.fetchall():
+        print(row)
+
+    #close connection to database
+    conn.close()
 
     return render(HttpRequest, "myTemplates/fileimport.html")
-
-
