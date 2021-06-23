04b55035e2b4e78cfcc4f9c9b21e3ec0a181682a
Marco <marco.thiessen@stud-mail.uni-wuerzburg.de>
2021-06-23 14:37:42 +0200

    Export dataframe to showData template

--- theShowDataApp/views.py
+++ theShowDataApp/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render
 import sqlite3
+import pandas as pd
+# import datatable as dt
 
 # Create your views here.
 
 def showdata_view(httprequest, *args, **kwargs):
     """Do  anything with request"""
 
-    conn = sqlite3.connect('TestDB1.db')
-    c = conn.cursor()
 
-    c.execute('''  
-        SELECT * FROM FRAUDS
-                  ''')
+    conn = sqlite3.connect('TestDB1.db')
+
+    '''
+    c = conn.cursor()
+    c.execute(   
+        #SELECT * FROM FRAUDS)
 
     showData=c.fetchall()
-    context = {
-        'showData': showData
-    }
+    '''
+    # Use an pandas dataframe as an import file for datatables
+    df = pd.read_sql_query("SELECT * FROM FRAUDS", conn)
 
-    return render(httprequest, "myTemplates/showdata.html", context)
+    context = {
+        'showData': df
+    }
+    conn.close()
+
+    print(context)
+
+    return render(httprequest, "myTemplates/showdata.html", context=context)
