4e3af1b3bc9fa22e03f10c1c0601c9f0241c861c
Marco <marco.thiessen@stud-mail.uni-wuerzburg.de>
2021-06-23 23:36:49 +0200

    Changed datatable format

--- theShowDataApp/views.py
+++ theShowDataApp/views.py
@@ -24,10 +24,20 @@ def showdata_view(httprequest, *args, **kwargs):
     context = {
         "showData": df,
         "test": "Hello World",
-        "data": df.to_html
+        # edit datatable
+        "data": df.to_html(classes="display table table-striped table-hover", table_id="dataShowTable", index=False,
+                           justify="center", header=True, )  # classes="table table-bordered"
     }
     conn.close()
 
     print(context)
 
     return render(httprequest, "myTemplates/showdata.html", context)
+
+
+def readtable_view(httprequest, *args, **kwargs):
+    readtable = pd.read_html("myTemplates/showdata.html")
+    #if(readtable.count() >1):
+    df1 = readtable[0]
+
+    return render(httprequest, "myTemplates/showdata.html")
