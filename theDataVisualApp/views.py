5008d30054a3714d51f6e2c88a40b0b976ba63a8
Julia Pfister <julia.pfister11@web.de>
2021-08-02 16:28:03 +0200

    Installation und Integration Daten-Visualisierung

--- theDataVisualApp/views.py
+++ theDataVisualApp/views.py
@@ -5,3 +5,38 @@ from django.shortcuts import render
 def datavisu_view(httprequest, *args, **kwargs):
     """Do  anything with request"""
     return render(httprequest, "myTemplates/data-visualization.html")
+#npm
+npm install chart.js
+
+#Script Tag
+< script
+src = "path/to/chartjs/dist/chart.js" > < / script >
+< script >
+var
+myChart = new
+Chart(ctx, {...});
+< / script >
+
+
+#Common JS
+var Chart = require('chart.js');
+var myChart = new Chart(ctx, {...});
+
+
+# Bundlers (Webpack, Rollup, etc.)
+import Chart from 'chart.js/auto';
+
+# Require JS
+require(['path/to/chartjs/dist/chart.min.js'], function(Chart){
+    var myChart = new Chart(ctx, {...});
+});
+
+require(['chartjs'], function(Chart) {
+    require(['moment'], function() {
+        require(['chartjs-adapter-moment'], function() {
+            new Chart(ctx, {...});
+        });
+    });
+});
+
+
